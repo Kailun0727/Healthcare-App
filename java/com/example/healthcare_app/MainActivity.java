f5c985f22a099db9814ff8dd47590cfd5c85d906
HYTan0909 <87137966+HYTan0909@users.noreply.github.com>
2022-06-09 14:31:24 +0800

    Add files via upload

--- java/com/example/healthcare_app/MainActivity.java
+++ java/com/example/healthcare_app/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.healthcare_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+
+    }
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.firstFragment, firstFragment).commit();
+                return true;
+
+            case R.id.community:
+                getSupportFragmentManager().beginTransaction().replace(R.id.secondFragment, secondFragment).commit();
+                return true;
+
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.thirdFragment, thirdFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
