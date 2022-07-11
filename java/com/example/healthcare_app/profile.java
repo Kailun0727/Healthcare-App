7206c95b8caed1bb4100dbc7e840fabc6fba5244
HYTan0909 <87137966+HYTan0909@users.noreply.github.com>
2022-07-11 15:17:57 +0800

    Minor update in user points function

--- java/com/example/healthcare_app/profile.java
+++ java/com/example/healthcare_app/profile.java
@@ -3,25 +3,29 @@ package com.example.healthcare_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Toast;
 
 public class profile extends AppCompatActivity {
-    int getPoints;
+    int points;
     String getId;
     String getName;
     TextView name;
+    DBHandler dbhandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        getId = getIntent().getStringExtra("id");
         getName = getIntent().getStringExtra("name");
         name = findViewById(R.id.name);
         name.setText("Hi, " + getName);
+        dbhandler = new DBHandler(this);
     }
 
     public void clicked_community(View view) {
@@ -30,8 +34,15 @@ public class profile extends AppCompatActivity {
     }
 
     public void clicked_points(View view) {
-        getPoints = getIntent().getIntExtra("points", 0);
-        Toast.makeText(this,"You have " + Integer.toString(getPoints) + " points. ", Toast.LENGTH_LONG).show();
+
+        Cursor c = dbhandler.readPoints(getId);
+        if (c.getCount() == 1){
+            while (c.moveToNext()) {
+                points = c.getInt(3);
+            }
+        }
+
+        Toast.makeText(this,"You have " + Integer.toString(points) + " points. ", Toast.LENGTH_LONG).show();
     }
 
     public void clicked_menu(View view) {
