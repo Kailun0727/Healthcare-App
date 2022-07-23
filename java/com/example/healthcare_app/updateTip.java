5696606fd88e5ac4dc57ee4634c3b0e11fbaf640
Kailun0727 <83753090+Kailun0727@users.noreply.github.com>
2022-07-23 13:17:25 +0800

    Add files via upload

--- java/com/example/healthcare_app/updateTip.java
+++ java/com/example/healthcare_app/updateTip.java
@@ -34,13 +34,19 @@ public class updateTip extends AppCompatActivity {
 
         tip = tip_et.getText().toString();
 
-        boolean updateTip = dbHandler.updateTip(id,tip);
-
-        if(updateTip == true){
-            Toast.makeText(this,"Updated Successfully",Toast.LENGTH_SHORT).show();
+        if(!tip_et.getText().toString().isEmpty()){
+            boolean updateTip = dbHandler.updateTip(id,tip);
+            if(updateTip == true){
+                Toast.makeText(this,"Updated Successfully",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this,"Failed to update",Toast.LENGTH_SHORT).show();
+            }
         }else{
-            Toast.makeText(this,"Failed to update",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Tip cannot be empty",Toast.LENGTH_SHORT).show();
         }
+
+
+
     }
 
 }
